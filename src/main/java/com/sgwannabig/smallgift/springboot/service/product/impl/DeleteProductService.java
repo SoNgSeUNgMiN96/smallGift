2a33a0d277c1a1f3849fd7d3092a2f590b900619
hojoon <ujuj0202@gmail.com>
2022-11-23 21:27:41 +0900

    fix: s3에 있는 이미지도 삭제하는 로직 추가

--- src/main/java/com/sgwannabig/smallgift/springboot/service/product/impl/DeleteProductService.java
+++ src/main/java/com/sgwannabig/smallgift/springboot/service/product/impl/DeleteProductService.java
@@ -4,6 +4,7 @@ import com.sgwannabig.smallgift.springboot.config.advice.exception.ProductNotFou
 import com.sgwannabig.smallgift.springboot.domain.product.Product;
 import com.sgwannabig.smallgift.springboot.repository.ProductRepository;
 import com.sgwannabig.smallgift.springboot.service.product.DeleteProductUsecase;
+import com.sgwannabig.smallgift.springboot.util.aws.S3Manager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class DeleteProductService implements DeleteProductUsecase {
 
   private final ProductRepository productRepository;
+  private final S3Manager s3Manager;
 
   @Override
   public void deleteProduct(Long productId) {
-    productRepository.delete(findProduct(productId));
+    Product foundedProduct = findProduct(productId);
+    s3Manager.deleteFile(foundedProduct.getProductImage());
+    productRepository.delete(foundedProduct);
   }
 
   private Product findProduct(Long productId) {
